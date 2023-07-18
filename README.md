b197259168a700b089cc25da4c720f9b3dd299e3
Zebreus <lennarteichhorn@googlemail.com>
2023-07-18 12:02:57 +0200

    Add nix flake

--- README.md
+++
@@ -1,2 +0,0 @@
-# rust_hls
-Real high-level synthesis from Rust using bambu
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "High-level synthesis from Rust";
+
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:zebreus/nixpkgs?rev=570ffa9c6529d5157153a8562e16e6e4a7ecb636";
+    old-nixpkgs.url = "nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, fenix, flake-utils, old-nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      rec {
+        old-pkgs = import old-nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; };
+
+        name = "rust_hls";
+
+        packages.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              # For nix
+              pkgs.nixpkgs-fmt
+              pkgs.nil
+
+              # For rust
+              (fenix.packages.${system}.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+              fenix.packages.${system}.rust-analyzer
+              pkgs.lldb_14
+
+              # For rust_hls
+              bambu
+              old-pkgs.verilator
+              jq
+            ];
+          };
+      }
+    );
+}
