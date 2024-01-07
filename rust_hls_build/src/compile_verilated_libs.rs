e7bbbbe1d3cd752cb0871534d6b97917a1fd9e09
Zebreus <lennarteichhorn@googlemail.com>
2024-01-07 23:01:56 +0100

    Add support for multiple modules in one crate

--- rust_hls_build/src/compile_verilated_libs.rs
+++ rust_hls_build/src/compile_verilated_libs.rs
@@ -72,126 +72,72 @@ fn get_lib_files(verilated_lib_path: &Path) -> Result<Vec<PathBuf>, CompileVeril
     Ok(files)
 }
 
-pub fn compile_verilated_module(
-    verilated_module_path: &Path,
-    cpp_shim_path: &Path,
-    verilated_lib_path: &Path,
-    top_module: &str,
-) -> Result<(), CompileVerilatedLibsError> {
-    let mut files = get_verilated_module_files(verilated_module_path)?;
-    let mut lib_files = get_lib_files(verilated_lib_path)?;
-    files.append(&mut lib_files);
-    files.push(cpp_shim_path.into());
-
-    let mut include_dirs = get_lib_include_dirs(verilated_lib_path)?;
-    include_dirs.push(verilated_module_path.into());
-
-    let mut cpp_cfg = cc::Build::new();
-    cpp_cfg.cpp(true).define("VL_PRINTF", "printf");
-    cpp_cfg.opt_level(3);
-
-    cpp_cfg.flag("-pthread");
-
-    let tool = cpp_cfg.get_compiler();
-    if tool.is_like_clang() {
-        cpp_cfg
-            .flag("-faligned-new")
-            .flag("-fbracket-depth=4096")
-            .flag("-Qunused-arguments")
-            .flag("-Wno-parentheses-equality")
-            .flag("-Wno-sign-compare")
-            .flag("-Wno-uninitialized")
-            .flag("-Wno-unused-parameter")
-            .flag("-Wno-unused-variable")
-            .flag("-Wno-shadow");
-    }
-    if tool.is_like_gnu() {
-        cpp_cfg
-            .flag("-std=gnu++17")
-            .flag("-faligned-new")
-            .flag("-Wno-bool-operation")
-            .flag("-Wno-sign-compare")
-            .flag("-Wno-uninitialized")
-            .flag("-Wno-unused-but-set-variable")
-            .flag("-Wno-unused-parameter")
-            .flag("-Wno-unused-variable")
-            .flag("-Wno-shadow");
-    }
-
-    include_dirs.into_iter().for_each(|dir| {
-        cpp_cfg.include(dir);
-    });
-
-    files.into_iter().for_each(|file| {
-        cpp_cfg.file(file);
-    });
-
-    cpp_cfg.define("VM_TRACE", "1");
-
-    cpp_cfg.compile(&format!("V{}__ALL", top_module));
-
-    return Ok(());
+pub struct Compiler {
+    pub compiler: cc::Build,
 }
 
-#[allow(dead_code)]
-pub fn compile_verilated_libs(verilated_lib_path: &Path) -> Result<(), CompileVerilatedLibsError> {
-    let (major, minor) = (4, 110);
-    println!("cargo:rustc-cfg=verilator_version=\"{}.{}\"", major, minor);
-    println!("cargo:rustc-cfg=verilator=\"flush_and_exit_cb\"");
+impl Compiler {
+    pub fn new(verilated_lib_path: &Path) -> Self {
+        let mut cpp_cfg = cc::Build::new();
+        cpp_cfg.cpp(true).define("VL_PRINTF", "printf");
+        cpp_cfg.opt_level(3);
+        cpp_cfg.flag("-pthread");
 
-    let files: Vec<PathBuf> = get_lib_files(verilated_lib_path)?;
+        let tool = cpp_cfg.get_compiler();
+        if tool.is_like_clang() {
+            cpp_cfg
+                .flag("-faligned-new")
+                .flag("-fbracket-depth=4096")
+                .flag("-Qunused-arguments")
+                .flag("-Wno-parentheses-equality")
+                .flag("-Wno-sign-compare")
+                .flag("-Wno-uninitialized")
+                .flag("-Wno-unused-parameter")
+                .flag("-Wno-unused-variable")
+                .flag("-Wno-shadow");
+        }
+        if tool.is_like_gnu() {
+            cpp_cfg
+                .flag("-std=gnu++17")
+                .flag("-faligned-new")
+                .flag("-Wno-bool-operation")
+                .flag("-Wno-sign-compare")
+                .flag("-Wno-uninitialized")
+                .flag("-Wno-unused-but-set-variable")
+                .flag("-Wno-unused-parameter")
+                .flag("-Wno-unused-variable")
+                .flag("-Wno-shadow");
+        }
 
-    let mut cfg = cc::Build::new();
-    let tool = cfg.get_compiler();
-    cfg.cpp(true);
-    cfg.opt_level(3);
+        let files = get_lib_files(verilated_lib_path).unwrap();
+        let include_dirs = get_lib_include_dirs(verilated_lib_path).unwrap();
 
-    if tool.is_like_clang() {
-        cfg.flag("-faligned-new")
-            .flag("-fbracket-depth=4096")
-            .flag("-Qunused-arguments")
-            .flag("-Wno-parentheses-equality")
-            .flag("-Wno-sign-compare")
-            .flag("-Wno-uninitialized")
-            .flag("-Wno-unused-parameter")
-            .flag("-Wno-unused-variable")
-            .flag("-Wno-shadow");
+        include_dirs.into_iter().for_each(|dir| {
+            cpp_cfg.include(dir);
+        });
+
+        files.into_iter().for_each(|file| {
+            cpp_cfg.file(file);
+        });
+
+        cpp_cfg.define("VM_TRACE", "1");
+
+        return Self { compiler: cpp_cfg };
     }
-    if tool.is_like_gnu() {
-        cfg.flag("-std=gnu++17")
-            .flag("-faligned-new")
-            .flag("-Wno-bool-operation")
-            .flag("-Wno-sign-compare")
-            .flag("-Wno-uninitialized")
-            .flag("-Wno-unused-but-set-variable")
-            .flag("-Wno-unused-parameter")
-            .flag("-Wno-unused-variable")
-            .flag("-Wno-shadow");
+
+    pub fn add_verilated_module(&mut self, verilated_module_path: &Path, cpp_shim_path: &Path) {
+        let mut files = get_verilated_module_files(verilated_module_path).unwrap();
+        files.push(cpp_shim_path.into());
+        files.into_iter().for_each(|file| {
+            self.compiler.file(file);
+        });
+
+        self.compiler.include(verilated_module_path);
+
+        // cpp_cfg.compile(&format!("V{}__ALL", top_module));
     }
 
-    cfg.define("VERILATOR_VERSION_MAJOR", major.to_string().as_str())
-        .define("VERILATOR_VERSION_MINOR", minor.to_string().as_str());
-
-    let include_dirs = get_lib_include_dirs(verilated_lib_path)?;
-    include_dirs.iter().for_each(|include| {
-        cfg.include(include);
-    });
-    cfg.files(files);
-    cfg.compile("verilated_libs_cc");
-
-    return Ok(());
-}
-
-#[cfg(test)]
-mod tests {
-    // #[test]
-    // fn compile_verilated_libs_works() {
-    //     let dir = TempDir::new().unwrap();
-
-    //     let target_dir = dir.path().join("output");
-
-    //     compile_verilated_libs(&target_dir).unwrap();
-
-    //     dir.close().unwrap();
-    // }
+    pub fn compile(&mut self) {
+        self.compiler.compile("rust-hls-verilated");
+    }
 }
